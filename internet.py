c65a76a839608cabb5fd06218ce8f90237771b82
Alung12338 <64249260+Alung12338@users.noreply.github.com>
2020-05-06 13:40:36 +0700

    Add files via upload

---
+++ internet.py
@@ -0,0 +1,19 @@
+# -- coding: utf-8 --
+import os
+import sys
+import time
+
+os.system('clear')
+print "\33[31;1m========================================="
+print "\33[33;1m author : alung"
+print "\33[0;32m youtube: Alung official"
+print "\33[31;1m Wa     : 081391460253"
+print "\33[33;1m Github : https://github.com/Alung12338"
+print "\33[0;32m========================================="
+time.sleep(7)
+os.system('ping 1.1.1.1')
+
+a = 0
+while (a < 3):
+  print ("1ms")
+  print ("😎😎😎")
